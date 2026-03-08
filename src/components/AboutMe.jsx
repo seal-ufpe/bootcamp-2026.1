cc9e4c77b75e446957b1dd4cd53594731ce20d35
Pedro <pedroh7.brito@gmail.com>
2026-03-08 14:14:56 -0300

    feat: adiciona página de perfil interativa em react

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,17 @@
+export default function AboutMe() {
+    return (
+        <section className="section-description">
+                <h2>About Me</h2>
+                <article className="aside-image">
+                    <article>
+                        <p>Currently, I'm studying Information Systems at UFPE (Federal University of Pernambuco), where I'm also a member of the Software Engineering Academic League (SEAL) in the Communication Department.</p>
+                        <p>Outside of that, I work as an assistant and social media manager at Prático Estudos, a private preparatory course. I also have practical experience with development in Python, C++, JavaScript, HTML, CSS and React.</p>
+                        <p>I am proficient in English, Canva and Excel, with a strong interest in areas such as Artificial Intelligence, Writing and UX Design.</p>
+                    </article>
+                    <aside>
+                        <img src="src\assets\assets-pedro\PedroIcon.png" alt="Profile Photo" />
+                    </aside>
+                </article>
+        </section>
+    )
+}
